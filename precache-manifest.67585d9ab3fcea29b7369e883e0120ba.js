650717b54061804adf8823a084079a452750306d
xiaodeaux <xiaodeaux@gmail.com>
2019-05-03 20:58:06 -0500

    init

---
+++ precache-manifest.67585d9ab3fcea29b7369e883e0120ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "fb841a6855291f896524",
+    "url": "/css/app.1ebbd5cc.css"
+  },
+  {
+    "revision": "fb841a6855291f896524",
+    "url": "/js/app.ef7a7895.js"
+  },
+  {
+    "revision": "5f07995f698153210fd1",
+    "url": "/js/chunk-vendors.9868ded3.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7e518afc5f908cc478195d2776508c31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
